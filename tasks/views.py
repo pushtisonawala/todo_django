51372bc491721f6925259bf091563d0034a396b4
pushtisonawala <168925733+pushtisonawala@users.noreply.github.com>
2024-12-09 23:51:45 +0530

    django todo

---
+++ tasks/views.py
@@ -0,0 +1,19 @@
+
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()  
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect('task_list') 
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
